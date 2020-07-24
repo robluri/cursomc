e248c4009e0a6d78a4130eefd6087c2b5221e6c6
Robson Luiz Ribeiro Moreira <robson.rmoreira@gmail.com>
2020-07-23 23:06:38 -0300

    Implementacao do banco de dados H2

--- src/main/java/com/robsonmoreira/cursomc/domain/Categoria.java
+++ src/main/java/com/robsonmoreira/cursomc/domain/Categoria.java
@@ -2,11 +2,24 @@ package com.robsonmoreira.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GeneratorType;
+
+@Entity 
+
 public class Categoria implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Integer id;
 	private String nome;
 
--- src/main/java/com/robsonmoreira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/robsonmoreira/cursomc/resources/CategoriaResource.java
@@ -18,10 +18,12 @@ public class CategoriaResource {
 		
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat3 = new Categoria(3, "Fabrica");
 		
 		List<Categoria> lista = new ArrayList<>();
 		lista.add(cat1);
 		lista.add(cat2);
+		lista.add(cat3);
 		return lista;
 	}
 		
