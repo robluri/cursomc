516f44bdc73fb961bb2351c22f961de5c1a1b8c0
Robson Luiz Ribeiro Moreira <robson.rmoreira@gmail.com>
2020-07-26 23:43:34 -0300

    Repository e service para Categoria

--- src/main/java/com/robsonmoreira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/robsonmoreira/cursomc/resources/CategoriaResource.java
@@ -1,30 +1,27 @@
 package com.robsonmoreira.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.robsonmoreira.cursomc.domain.Categoria;
+import com.robsonmoreira.cursomc.services.CategoriaService;
 
 @RestController 
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		Categoria cat3 = new Categoria(3, "Fabrica");
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);		
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		lista.add(cat3);
-		return lista;
 	}
 		
 }
